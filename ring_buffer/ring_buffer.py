f5267ba7b742e394252783cb893a6d3973a79bf9
Dylan Collins <dcollins5590@students.mc3.edu>
2020-07-17 13:51:04 -0400

    still didnt finish the sprint

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,30 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.storage = []
+        self.head = None
+        self.tail = None
+        self.length = 0
     def append(self, item):
-        pass
+        if self.length == 0:
+                self.storage.append(item)
+                self.head = item
+                self.tail = self.head
+                self.length += 1
+
+        elif self.length >= 1 and self.length < self.capacity:
+                self.storage.append(item)
+                self.tail = item
+                self.length += 1
+
+        else:
+            replace_this_element = self.storage.index(self.head)
+            self.storage[replace_this_element] = item
+            self.tail = item
+            if replace_this_element + 1 == self.capacity:
+                self.head = self.storage[0]
+            else:
+                self.head = self.storage[replace_this_element + 1]
 
     def get(self):
-        pass
+        return self.storage
